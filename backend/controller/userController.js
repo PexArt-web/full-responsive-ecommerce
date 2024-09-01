64f7af3c2be96027c3d0e52bcefe80be163a93e4
PexArt-web <pexart74@gmail.com>
2024-09-01 01:47:38 +0100

    auth

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -5,12 +5,12 @@ const signUpUser = async (req, res) => {
   const { username, firstName, lastName, email, password } = req.body;
 
   try {
-    const signup = await User.create({ ...req.body });
-    res.status(200).json({ success: "signing up user" });
+    const signup = await User.signup(username, email, password, firstName, lastName)
+    res.status(200).json({ success: "signing up user" , user: email, signup});
   } catch (error) {
     res
       .status(400)
-      .json({ message: `errror signing up user: ${error.message}` });
+      .json({ message: `Errror signing up user: ${error.message}` });
   }
 };
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const saltRound = 10;
+const validator = require("validator");
 
 // Define the user schema
 
@@ -26,6 +29,54 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+// static sign up method
+
+userSchema.statics.signup = async function (
+  username,
+  email,
+  password,
+  firstName,
+  lastName
+) {
+  try {
+    if (!email || !password || !firstName || !lastName || !username) {
+      throw new Error(`all fields must be filled`);
+    }
+   if(!validator.isEmail(email)){
+     throw new Error(`Invalid email format: ${email}`)
+   }
+   if (!validator.isStrongPassword(password)){
+     throw new Error(`Password not strong enough`)
+   }
+
+    const existingUser = await this.findOne({ email });
+    if (existingUser) {
+      throw new Error(`User ${email} already exists`);
+    }
+
+    // generate salt for password hashing
+
+    const salt = await bcrypt.genSalt(saltRound);
+
+    // hash the password with the generated salt for security purposes
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // creating or signing up new user
+    const newUser = await this.create({
+      username: username,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    });
+
+    return newUser;
+  } catch (error) {
+    throw new Error(`${error.message}`);
+  }
+};
 
 const user = mongoose.model("User", userSchema);
 
